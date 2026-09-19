4e535788740bc444a264d41ace88a044442a5fc7
Chloroform <icyred21023@gmail.com>
2026-09-19 18:16:56 -0400

    s

--- _Mastery_Sheet_Vector_Editor_V8.py
+++ _Mastery_Sheet_Vector_Editor_V8.py
@@ -1431,7 +1431,7 @@ def main() -> None:
 
     for folder_idx, (folder_path, folder_name, hero_name, png_paths) in enumerate(folders, start=1):
         print(folder_name)
-        if folder_name not in ["1049", "1054"]:
+        if folder_name not in ["1011"]:
             continue
         print(
             f"[{folder_idx}/{len(folders)}] HERO FOLDER: {folder_name} "
